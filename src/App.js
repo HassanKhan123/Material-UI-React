7a07ac222e65bd08b15df875763241548571c88b
HassanKhan123 <hk2602809@gmail.com>
2020-10-10 14:05:06 +0500

    material ui checkbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { Button, ButtonGroup } from '@material-ui/core';
-import { Save,Delete } from '@material-ui/icons';
+import React, { useState } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+} from '@material-ui/core';
+import { Save, Delete } from '@material-ui/icons';
+
+function CheckboxComponent() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox checked={checked} onChange={() => setChecked(!checked)} />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
-    <div>
+    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+      <CheckboxComponent />
       <ButtonGroup>
         <Button
           variant='contained'
