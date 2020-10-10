8671efb33bf81df98ba9b174215222c4ac58b2bb
HassanKhan123 <hk2602809@gmail.com>
2020-10-10 13:54:00 +0500

    material-ui button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Button, ButtonGroup } from '@material-ui/core';
+import { Save,Delete } from '@material-ui/icons';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ButtonGroup>
+        <Button
+          variant='contained'
+          color='primary'
+          size='large'
+          startIcon={<Save />}
+        >
+          Save
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='large'
+          startIcon={<Delete />}
+        >
+          Discard
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
 }
 
 export default App;
